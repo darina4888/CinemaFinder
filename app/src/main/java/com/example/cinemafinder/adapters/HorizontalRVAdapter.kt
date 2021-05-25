f07e2f2ec72495cbf9cdaa096b5716a4f8e2c2c7
darina4888 <daria_fedoseeva@mail.ru>
2021-05-25 08:20:28 +0300

    step4

--- app/src/main/java/com/example/cinemafinder/adapters/HorizontalRVAdapter.kt
+++ app/src/main/java/com/example/cinemafinder/adapters/HorizontalRVAdapter.kt
@@ -9,18 +9,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cinemafinder.R
+import com.example.cinemafinder.model.HorizontalRVModel
+import com.example.cinemafinder.ui.details.DetailsActivity
+import kotlinx.android.synthetic.main.horizontal_item.view.*
 
 class HorizontalRVAdapter(private var mContext: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var itemsList: ArrayList<HorizontalRVModel> = ArrayList()
     private var mRowIndex = -1
-    //step7
-    fun setColorsData(data: ArrayList<HorizontalRVModel>) {
-        if (itemsList != data) {
-            itemsList = data
-            notifyDataSetChanged()
-        }
-    }
+
 
     fun setRowIndex(index: Int) {
         mRowIndex = index
@@ -39,8 +37,8 @@ class HorizontalRVAdapter(private var mContext: Context) : RecyclerView.Adapter<
     }
     //step8
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        holder.itemView.item_image.setBackgroundColor(Color.parseColor(itemsList[position].color))
-        holder.itemView.item_title.text = itemsList[position].name
+
+        holder.itemView.item_title.text = itemsList[position].title
         holder.itemView.setOnClickListener {
             Log.d(
                     "TAG",
@@ -48,8 +46,8 @@ class HorizontalRVAdapter(private var mContext: Context) : RecyclerView.Adapter<
             )
             val intent = Intent(mContext, DetailsActivity::class.java)
             val bundle = Bundle()
-            bundle.putString("detail_color", itemsList[position].color)
-            bundle.putString("detail_name", itemsList[position].name)
+
+            bundle.putString("detail_name", itemsList[position].title)
             intent.putExtras(bundle)
             mContext.startActivity(intent)
         }
--- app/src/main/java/com/example/cinemafinder/adapters/VerticalRVAdapter.kt
+++ app/src/main/java/com/example/cinemafinder/adapters/VerticalRVAdapter.kt
@@ -1,11 +1,16 @@
 package com.example.cinemafinder.adapters
 
+import android.graphics.Color
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cinemafinder.R
 import com.example.cinemafinder.model.HorizontalRVModel
+import io.github.luizgrp.sectionedrecyclerviewadapter.Section
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionParameters
+import kotlinx.android.synthetic.main.vertical_header.view.*
 
-class VerticalRVAdapter(private val category: String, private val films: ArrayList<HorizontalRVModel>) {
+class VerticalRVAdapter(private val category: String, private val films: ArrayList<HorizontalRVModel>) : Section(SectionParameters.builder().itemResourceId(R.layout.vertical_item).headerResourceId(R.layout.vertical_header).build()){
 
     class ItemViewHolder(rootView: View) : RecyclerView.ViewHolder(rootView) {
         private val horizontalRecyclerView: RecyclerView
@@ -16,24 +21,35 @@ class VerticalRVAdapter(private val category: String, private val films: ArrayLi
             horizontalRecyclerView = itemView.findViewById(R.id.horizontal_rv)
             horizontalRecyclerView?.layoutManager =
                     LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-            horizontalRecyclerView?.addItemDecoration(
-                    EqualSpacingItemDecoration(
-                            8,
-                            EqualSpacingItemDecoration.HORIZONTAL
-                    )
-            )
+
             horizontalAdapter = HorizontalRVAdapter(context)
             horizontalRecyclerView.adapter = horizontalAdapter
         }
     }
 
+    class SectionViewHolder(rootView: View) : RecyclerView.ViewHolder(rootView)
+
+    override fun getHeaderViewHolder(view: View): RecyclerView.ViewHolder {
+        return SectionViewHolder(view)
+    }
+
+    override fun getContentItemsTotal(): Int {
+        return films.size
+    }
+
+    override fun getItemViewHolder(view: View): RecyclerView.ViewHolder {
+        return ItemViewHolder(view)
+    }
+
+    override fun onBindHeaderViewHolder(holder: RecyclerView.ViewHolder) {
+        holder.itemView.vertical_category_text.setTextColor(Color.WHITE)
+        holder.itemView.vertical_category_text.text = category //category1
+    }
+
+
     override fun onBindItemViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val itemHolder = holder as ItemViewHolder
-        // Subcategory
-        itemHolder.itemView.subcategory_text.text = subcategory[position]
-        // Color items
-        itemHolder.horizontalAdapter.setColorsData(colors[position])
-        // Pass the current row
+
         itemHolder.horizontalAdapter.setRowIndex(position)
     }
 }
